3134c66503236c6630de1be5b143ffb90a9b197b
judevv <judevv604@gmail.com>
2023-07-12 18:54:52 +0900

    Footer 섹션 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.js';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Footer />
     </BrowserRouter>
   );
 }
