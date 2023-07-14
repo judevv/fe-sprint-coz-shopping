04d2c8b0fdb08a321ced8d72e347d996ffcc1601
judevv <judevv604@gmail.com>
2023-07-14 11:04:53 +0900

    CardItem 표출 및 api 데이터 받아오기

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 import { styled } from "styled-components";
 
 const CardItemWrapper = styled.section`
@@ -7,26 +7,52 @@ const CardItemWrapper = styled.section`
     width: 264px;
     height: 264px;
 `
-
-const CardItemImg = styled.div`
+const CardItemImgWrapper = styled.div`
+    border: 1px solid red;
+    border-radius: 12px;
     width: 264px;
     height: 210px;
-    border: 1px solid black;
-    border-radius: 12px;
+
+    & > img {
+        overflow: hidden;
+    }
 `
 
+const CardItemContentWrapper = styled.div`
+`
 
 function CardItem () {
+    const [ itemData, setItemData ] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://cozshopping.codestates-seb.link/api/v1/products')
+            .then(res => setItemData(res.data))
+            .catch(err => console.log(err))
+    }, []);
+
     return (
         <CardItemWrapper>
-            <CardItemImg>이미지</CardItemImg>
-            <div>
-                <span>상품이름</span>
-                <span>할인율</span>
-                <span>상품가격</span>
-            </div>
+            <CardItemImgWrapper>
+                <img src={itemData.type === "Brand" ? itemData.brand_image_url : itemData.image_url} />
+            </CardItemImgWrapper>
+            <CardItemContentWrapper>
+                <div>{itemData.type === "Category" ? `#${itemData.title}` : itemData.type === "Brand" ? `${itemData.brand_name}` : `${itemData.title}`}</div>
+                <div>{itemData.type === "Product" ? `${itemData.discountPercentage}` : itemData.type === "Brand" ? `관심고객수` : null}</div>
+                <div>{itemData.type === "Exhibition" ? `${itemData.sub_title}` : null}</div>
+                <div>{itemData.type === "Product" ? `${itemData.price}원` : itemData.type === "Brand" ? `${itemData.follower}` : null}</div>
+            </CardItemContentWrapper>
         </CardItemWrapper>
     )
 }
 
 export default CardItem;
+/*
+{itemData && itemData.map((item) => {
+                return (
+                    <div>
+                        {item.title}
+                        <img src={item.image_url} alt="" />
+                    </div>
+                )
+            })}
+*/
