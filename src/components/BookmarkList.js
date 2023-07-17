c3ffa4bb1f90be5dbc3ac51eb2f9c7a5058ddc72
judevv <judevv604@gmail.com>
2023-07-17 11:26:33 +0900

    뭘 수정했는진 모르겠지만 커밋해야되는 상태가 됨

--- src/components/BookmarkList.js
+++ src/components/BookmarkList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { styled } from "styled-components";
 import CardItem from "./CardItem";
 
@@ -10,7 +10,6 @@ const BookmarkListContainer = styled.div`
         align-items: center;
         gap: 24px;
     }
-    
 `
 
 
