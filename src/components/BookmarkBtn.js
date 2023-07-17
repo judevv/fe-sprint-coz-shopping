027bcd4c4056e4e970cdd577b6b2136e8f2a5837
judevv <judevv604@gmail.com>
2023-07-17 21:55:37 +0900

    Toast 알림 구현

--- src/components/BookmarkBtn.js
+++ src/components/BookmarkBtn.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { ReactComponent as BookmarkOff } from '../assets/images/BookmarkOff.svg';
 import { ReactComponent as BookmarkOn } from '../assets/images/BookmarkOn.svg';
+import Toast from "./Toast";
+
 
 function BookmarkBtn ({ itemData }) {
     const [ isBookmarked, setIsBookmarked ] = useState(itemData.isBookmarked);
+    const [ toasts, setToasts ] = useState([]);
 
     useEffect(() => {
         const bookmarkState = localStorage.getItem("bookmarkState");
@@ -22,13 +25,54 @@ function BookmarkBtn ({ itemData }) {
         if (isBookmarked) {
             const updatedItems = bookmarkedItems.filter((id) => id !== itemData.id);
             localStorage.setItem("bookmarkState", JSON.stringify(updatedItems));
+            addToasts(
+                <div className="toastText">
+                    <BookmarkOff />상품이 북마크에서 제거되었습니다.
+                </div>
+              );
         } else {
             const updatedItems = [...bookmarkedItems, itemData.id];
             localStorage.setItem("bookmarkState", JSON.stringify(updatedItems));
+            addToasts(
+                <div className="toastText">
+                    <BookmarkOn />상품이 북마크에 추가되었습니다.
+                </div>
+              );
         }
 
         setIsBookmarked(!isBookmarked);
     }
+
+    function addToasts(message) {
+        const newToast = {
+            id: Date.now(),
+            message: message
+        };
+
+        setToasts((prevToasts) => [...prevToasts, newToast]);
+    }
+
+    useEffect(() => {
+        if (toasts.length > 0) {
+          const timer = setTimeout(() => {
+            removeToast(toasts[0].id);
+          }, 1000);
+
+          return () => clearTimeout(timer);
+        }
+      }, [toasts]);
+
+      function removeToast(id) {
+        setToasts((prevToasts) => {
+          const index = prevToasts.findIndex((toast) => toast.id === id);
+          if (index !== -1) {
+            const newToasts = [...prevToasts];
+            newToasts.splice(index, 1);
+            return newToasts;
+          }
+          return prevToasts;
+        });
+      }
 
         return (
         <div>
@@ -37,6 +81,14 @@ function BookmarkBtn ({ itemData }) {
             ) : (
                 <BookmarkOff onClick={bookmarkHandler} />
             )}
+            {toasts.map((toast, index) => (
+                <Toast
+                    key={toast.id}
+                    message={toast.message}
+                    visible={true}
+                    index={index}
+                    removeToast={removeToast} />
+            ))}
         </div>
     )
 }
